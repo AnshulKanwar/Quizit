58efbe88f55ee5211b00b20339ba87e6d9a818e1
Anshul Kanwar <4n5hu1@protonmail.com>
2022-10-31 23:21:09 +0530

    Initial Commit

---
+++ Quizit/QuizitApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizitApp.swift
+//  Quizit
+//
+//  Created by Anshul Kanwar on 31/10/22.
+//
+
+import SwiftUI
+
+@main
+struct QuizitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
